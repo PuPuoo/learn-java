0e2e77ea856892adf9a68dfb0ccb4288ad9a637a
PuPu <382124535@qq.com>
2025-08-02 12:31:20 +0800

    feat: 第一个Java代码

--- learn-java/src/chapter01/Java01_HelloWorld.java
+++ learn-java/src/chapter01/Java01_HelloWorld.java
@@ -3,6 +3,6 @@ package chapter01;
 
 public class Java01_HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World1234");
+        System.out.println("Hello World12345");
     }
 }
